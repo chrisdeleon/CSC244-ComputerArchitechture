d97f64f1e24f17630054078b1b42d2961798b7cf
Chris de Leon <95385991+chrisdeleon@users.noreply.github.com>
2023-04-13 18:43:13 -0500

    Add files via upload

--- linearSum/linearSum.s
+++ linearSum/linearSum.s
@@ -4,6 +4,11 @@
 DATA		DCD		9, 6, 3, 8, 5, 2, 7, 4, 1, 0
 		ADD		R4, R0, #0
 		ADD		R5, R0, #0x100
-LOOP
-
+LOOP1
+		LDR		R6, [R5, #0]
+		ADD		R7, R7, R6
+		ADD		R4, R4, #1
+		ADD		R5, R5, #4
+		CMP		R4, R2
+		BMI		LOOP1
 		
